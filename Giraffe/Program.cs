fb4a603e6db1a8fd2c5ec1cbee6f2e833b7ee507
dluttig <dluttig5@live.com>
2020-04-18 16:22:22 +1200

    Hello World!

---
+++ Giraffe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numResult = GetMax(5, 2);
+
+            Console.WriteLine(numResult);
+
+            Console.ReadLine();
+
+        }
+
+      static int GetMax(int num1, int num2)
+        {
+            int result = 0;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            } else if (num1 < num2)
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+
+    }
+}
